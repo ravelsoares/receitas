11578c3451f06ffe4edbd38a4f31402ff4025b46
ravelsoares <ravelsoares64@gmail.com>
2022-03-16 15:19:06 -0300

    First Commit

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:receitas/components/category_item.dart';
+import 'package:receitas/data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Vamos cozinhar ?'),
+        centerTitle: true,
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((e) => CategoryItem(e)).toList(),
+      ),
+    );
+  }
+}
